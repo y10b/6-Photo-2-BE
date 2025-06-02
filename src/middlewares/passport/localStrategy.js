8b9eaff8b126f7d8291f991a337107c08c4d46de
y10b <victory.jun01@gmail.com>
2025-06-02 17:15:26 +0900

    HOTFIX: 로그인 안되는 문제 해결

--- src/middlewares/passport/localStrategy.js
+++ src/middlewares/passport/localStrategy.js
@@ -1,5 +1,6 @@
-import {Strategy as LocalStrategy} from 'passport-local';
+import { Strategy as LocalStrategy } from 'passport-local';
 import userService from '../../services/userService.js';
+import bcrypt from 'bcrypt';
 
 const localStrategy = new LocalStrategy(
   {
@@ -9,11 +10,11 @@ const localStrategy = new LocalStrategy(
     try {
       const user = await userService.getUser(email, password);
       if (!user) {
-        return done(null, false, {message: '존재하지 않는 이메일입니다.'});
+        return done(null, false, { message: '존재하지 않는 이메일입니다.' });
       }
       const isMatch = await bcrypt.compare(password, user.encryptedPassword);
       if (!isMatch) {
-        return done(null, false, {message: '비밀번호가 일치하지 않습니다.'});
+        return done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
       }
       return done(null, user); // req.user = user; req.isAuthenticated() = true
     } catch (error) {
